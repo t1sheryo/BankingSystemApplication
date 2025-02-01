3c9cf2fd2373a212842ee1fbb5f07771c5cf2843
TIMOFEY CHEBERUK <timofey13052005@gmail.com>
2025-02-01 13:19:21 +0300

    initial commit

---
+++ app/src/test/java/com/bankingsystem/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankingsystem.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
